87aeb87095d592672c4bbc671ae14ce73aae8c83
jweatherly <jamesthebard@gmail.com>
2023-10-13 10:55:49 -0400

    Moved YAML > JSON conversion script

--- handbrake/schema/convert.py
+++ handbrake/schema/convert.py
@@ -7,8 +7,8 @@ import json
 # with open('schema/handbrake.schema.yaml', 'w') as f:
 #     yaml.dump(content, f, sort_keys=False, width=float("inf"))
 
-with open('handbrake/schema/handbrake.schema.yaml', 'r') as f:
+with open('schema/handbrake.schema.yaml', 'r') as f:
     content = yaml.safe_load(f)
 
-with open('handbrake/schema/handbrake.schema.json', 'w') as f:
+with open('schema/handbrake.schema.json', 'w') as f:
     json.dump(content, f, indent=2)
